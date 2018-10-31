525b04daba743e6dd0c337ee61efdafb27d63ea9
Nolan McCafferty <njma2016@pomona.edu>
2018-10-30 21:46:06 -0700

    Update max_height.py

--- max_height.py
+++ max_height.py
@@ -2,7 +2,7 @@
 import operator
 
 # read in infile and construct initial box types
-with open('infile.txt', 'r') as infile:
+with open('ps9TestFiles/blocks100in', 'r') as infile:
     num_types = int(infile.readline())
     box_types = []
     for line in infile:
@@ -24,40 +24,54 @@ for box_type in box_types:
 rotations.sort(key=operator.itemgetter('area'), reverse=True)
 
 def get_max_height(rotations):
-    # initialize max height for each box
-    max_heights = []
-    num_blocks = []
-    num_boxes = len(rotations)
-    blocks_used = []
+  # initialize max height for each box
+  max_heights = []
+  num_blocks = []
+  num_boxes = len(rotations)
+  blocks_used = []
     
-    for i in range(num_boxes):
-        num_blocks.append(0)
-        max_heights.append(rotations[i]['height'])
+  for i in range(num_boxes):
+      num_blocks.append(0)
+      max_heights.append(rotations[i]['height'])
     
     # calculate max stack heights from the bottom up 
-    for i in range(num_boxes):
-        max_heights[i] = 0
-        box = rotations[i]
-        blocks_used.append([])
-        height_rest = 0
-        blocks = []
+def get_max_height(rotations):
+  # initialize max height for each box
+  max_heights = []
+  num_blocks = []
+  num_boxes = len(rotations)
+  blocks_used = []
+    
+  for i in range(num_boxes):
+      num_blocks.append(0)
+      max_heights.append(rotations[i]['height'])
+    
+  # calculate max stack heights from the bottom up 
+  for i in range(num_boxes):
+    max_heights[i] = 0
+    height_rest = 0
+    block_num = 0
+    box = rotations[i]
+    blocks_used.append([])
+    blocks = []
         
-        for j in range(i):
-            previous_box = rotations[j]
-            if box['width'] < previous_box['width'] and box['depth'] < previous_box['depth'] and height_rest < max_heights[j]:
-                height_rest = max_heights[j]
-                num_blocks[i] += 1
-                blocks = blocks_used[j]
-        blocks_used[i] = blocks + [box]
-        max_heights[i] = height_rest + box['height']
-    max_height = max(max_heights)
-    index = max_heights.index(max_height);
-    print('The tallest tower has', num_blocks[index], 'blocks and a height of', max_height)
-    return max_height, blocks_used[index]
+    for j in range(i):
+      previous_box = rotations[j]
+      if box['width'] < previous_box['width'] and box['depth'] < previous_box['depth'] and height_rest < max_heights[j]:
+        height_rest = max_heights[j]
+        block_num = num_blocks[j]
+        blocks = blocks_used[j]
+    blocks_used[i] = blocks + [box]
+    max_heights[i] = height_rest + box['height']
+    num_blocks[i] = block_num + 1
+  max_height = max(max_heights)
+  index = max_heights.index(max_height);
+  print('The tallest tower has', num_blocks[index], 'blocks and a height of', max_height)
+  return max_height, num_blocks[index], blocks_used[index]
 
-    max_height, blocks_used = get_max_height(rotations)
+max_height, num_blocks, blocks_used = get_max_height(rotations)
 
-    with open('outfile.txt', 'w') as outfile:
-    outfile.write(str(num_types) + '\n')
-    for block in blocks_used:
-        outfile.write(str(block['depth']) + ' ' + str(block['width']) + ' ' + str(block['height']) + '\n')
+with open('ps9TestFiles/100testout.txt', 'w') as outfile:
+  outfile.write(str(num_blocks) + '\n')
+  for block in blocks_used:
+    outfile.write(str(block['depth']) + ' ' + str(block['width']) + ' ' + str(block['height']) + '\n')
